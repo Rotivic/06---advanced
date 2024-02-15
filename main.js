50043e223bd9ededca3f7ca45730251f2d8f19bc
Rotvic <victormfdezfdez@gmail.com>
2024-02-15 12:54:14 +0100

    Fin de priemra lección

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
+import { enviromentsComponent } from './src/concepts/01-enviroments';
 
 
 document.querySelector('#app').innerHTML = `
@@ -19,5 +20,8 @@ document.querySelector('#app').innerHTML = `
   </div>
 `;
 
+const element = document.querySelector('.card');
+
+enviromentsComponent(element);
 
 console.log('Hola Mundo');
